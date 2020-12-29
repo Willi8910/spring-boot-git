4ecc202dcc9526b3a72398b8a18c39f2500c5f0f
William <williamlie8910@gmail.com>
2020-12-29 15:52:04 +0700

    Test again

--- src/main/java/com/technical/git/controller/Controller.java
+++ src/main/java/com/technical/git/controller/Controller.java
@@ -1,6 +1,6 @@
 package com.technical.git.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/api/git")
 public class Controller {
