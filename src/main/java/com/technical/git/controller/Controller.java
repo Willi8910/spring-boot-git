eee98b3d6cba3d6484aea385c8d18fce6ce2cc33
William <williamlie8910@gmail.com>
2020-12-30 16:21:03 +0700

    perbaikan

--- src/main/java/com/technical/git/controller/Controller.java
+++ src/main/java/com/technical/git/controller/Controller.java
@@ -1,8 +1,19 @@
 package com.technical.git.controller;
 
+        import com.technical.git.service.GeneratorService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
         import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/api/git")
-public class Controller {
+        import java.io.IOException;
 
+@RestController()
+public class Controller {
+        @Autowired
+        GeneratorService generatorService;
+        @GetMapping("/generate")
+        public String generateString(@RequestParam int line) throws IOException {
+                return generatorService.generateFile(line);
+        }
 }
